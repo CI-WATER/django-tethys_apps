ceac5880a04fe5e0d233b2bafc7db4cb85aa7a8f
Nathan Swain <nathan.swain86@gmail.com>
2014-10-16 21:58:23 -0600

    Implemented the scaffold command.

--- tethys_apps/cli/__init__.py
+++ tethys_apps/cli/__init__.py
@@ -2,13 +2,16 @@
 import argparse
 import subprocess
 
+GEN_SETTINGS_OPTION = 'settings'
+VALID_GEN_OBJECTS = (GEN_SETTINGS_OPTION,)
+
 
 def scaffold_command(args):
     """
     Create a new Tethys app projects in the current directory.
     """
     PREFIX = 'tethysapp'
-    project_name = args.project_name
+    project_name = args.name
 
     if PREFIX not in project_name:
         project_name = '{0}-{1}'.format(PREFIX, project_name)
@@ -41,13 +44,13 @@ def tethys_command():
 
     # Setup scaffold parsers
     scaffold_parser = subparsers.add_parser('scaffold', help='Create a new Tethys app project from a scaffold.')
-    scaffold_parser.add_argument('project_name', help='The name of the new Tethys app project to create.')
+    scaffold_parser.add_argument('name', help='The name of the new Tethys app project to create.')
     scaffold_parser.set_defaults(func=scaffold_command)
 
     # Setup generate command
     gen_parser = subparsers.add_parser('gen', help='Create aids the setup of Tethys by automating '
                                                    'creation of supporting files.')
-    gen_parser.add_argument('type', help='The type of object to generate.')
+    gen_parser.add_argument('type', help='The type of object to generate.', choices=VALID_GEN_OBJECTS)
     gen_parser.set_defaults(func=generate_command)
 
     # Setup start server parsers
